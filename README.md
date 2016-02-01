74751f752443660a7a4c9b14973c8e68e6c0163d
Djailla <bastien.vallet@gmail.com>
2016-02-01 13:08:28 +0100

    Use the forked version of shellinabox which updated Use tag version 2.19

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ Shell In A Box - NAS OS App
 ==================================
 
 **Shell In A Box** is an app to install on NASes running NAS OS (for Seagate or LaCie NAS).
-It's based on the [shellinabox](https://code.google.com/archive/p/shellinabox/) project.
+It's based on the [shellinabox fork](https://github.com/shellinabox/shellinabox) project.
 
 It allow to access to a shell in a web ui.
--- com.djailla.shellinabox/build.sh
+++ com.djailla.shellinabox/build.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-mkdir -m 755 -p /opt/shellinabox
+mkdir -m 755 -p /opt/shellinabox/
 
-/bin/echo -e '#!/bin/sh\nexit 101' > /usr/sbin/policy-rc.d
-chmod +x /usr/sbin/policy-rc.d
-
+# Install dependencies
 apt-get update
-apt-get install -y -q --no-install-recommends shellinabox
+apt-get install  -y -q git libssl-dev libpam0g-dev zlib1g-dev dh-autoreconf
 
-rm /usr/sbin/policy-rc.d
+# Checkout sources in /tmp
+cd /tmp
+git clone https://github.com/shellinabox/shellinabox.git
+
+# Checkout last tag
+cd shellinabox
+git checkout tags/v2.19
+
+# Build the component
+autoreconf -i
+./configure && make
+
+# Copy the binary to /usr/bin
+cp /tmp/shellinabox/shellinaboxd /usr/bin
+
+# Cleanup
+rm -rf /tmp/shellinabox
 
 install -m 755 /home/source/rc.local /etc
 
